6214e0c4a26d57027e0478bf0ccfddea0b402cc8
Kyle Heppenstall <[kyle.heppenstall@gmail.com]>
2016-10-24 20:47:21 -0600

    Hello world working, no testing

--- lib/server.rb
+++ lib/server.rb
@@ -1,13 +1,46 @@
 require 'socket'
 
-server = TCPServer.new(9292)
-num = 0
-loop do
-  client = server.accept    
-  client.puts "Hello !"
-  client.puts "Time is #{Time.now}"
-  num += 1
-  string = "Hello, world (#{num})"
-  client.puts string
-  client.close
+class Server
+
+  attr_reader :server
+
+  def initialize
+    @server = TCPServer.new 9292
+  end
+
+  def hello_world
+    counter = 0
+    loop do
+      request_lines = []
+      client = server.accept 
+      while line = client.gets and !line.chomp.empty?
+        request_lines << line.chomp 
+      end
+      client.puts "Hello, world (#{counter})"
+      counter += 1
+      output_diagnostics(request_lines, client)
+      client.close
+    end
+  end
+
+  def output_diagnostics(request_lines, client)
+    verb = request_lines[0].split(' ')[0]
+    path = request_lines[0].split(' ')[1]
+    protocol = request_lines[0].split(' ')[2]
+    host = request_lines[1].split(':')[1].strip
+    port = request_lines[1].split(':')[2]
+    accept = request_lines[6].split(':')[1].strip
+    client.puts "<pre>"
+    client.puts "Verb: #{verb}"
+    client.puts "Path: #{path}"
+    client.puts "Protocol: #{protocol}"
+    client.puts "Host: #{host}"
+    client.puts "Port: #{port}"
+    client.puts "Accept: #{accept}"
+    client.puts "Origin: #{host}"
+    client.puts "</pre>"
+  end
+
 end
+
+Server.new.hello_world
--- test/server_test.rb
+++ test/server_test.rb
@@ -1,11 +1,22 @@
 require './lib/server'
 require "minitest/autorun"
 require "minitest/pride"
+require 'faraday'
 
 class ServerTest < Minitest::Test
 
-def test_it_exists
-  
+  def test_it_exists
+    assert Server.new
+  end
+
+
+
+  def test_hello_world_prints_with_1_request
+    Server.new.hello_world
+    response = Faraday.get 'http://127.0.0.1:9292/'
+    assert_equal "Hello, World (0)", response
+  end
+
 
 end
 
