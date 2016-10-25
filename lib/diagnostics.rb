a820c8f4f2716ea732ccd9836e30d820287f45d4
Kyle Heppenstall <[kyle.heppenstall@gmail.com]>
2016-10-24 21:29:31 -0600

    Includes parameters and Diagnostics class, no testing still

---
+++ lib/diagnostics.rb
@@ -0,0 +1,46 @@
+class Diagnostics
+
+  attr_reader :request_lines
+
+  def initialize(request_lines)
+    @request_lines = request_lines
+  end
+
+  def populate
+    diagnostics = []
+    diagnostics << "<pre>"
+    diagnostics << "Verb: #{verb}"
+    diagnostics << "Path: #{path}"
+    diagnostics << "Protocol: #{protocol}"
+    diagnostics << "Host: #{host}"
+    diagnostics << "Port: #{port}"
+    diagnostics << "Accept: #{accept}"
+    diagnostics << "Origin: #{host}"
+    diagnostics << "</pre>"
+  end
+
+  def verb
+    request_lines[0].split(' ')[0]
+  end
+
+  def path
+    request_lines[0].split(' ')[1]
+  end
+  
+  def protocol
+    request_lines[0].split(' ')[2]
+  end
+
+  def host
+    request_lines[1].split(':')[1].strip
+  end
+
+  def port
+    request_lines[1].split(':')[2]
+  end
+  
+  def accept
+    request_lines[6].split(':')[1].strip
+  end
+
+end
--- lib/server.rb
+++ lib/server.rb
@@ -1,4 +1,5 @@
 require 'socket'
+require './lib/diagnostics'
 
 class Server
 
@@ -6,9 +7,10 @@ class Server
 
   def initialize
     @server = TCPServer.new 9292
+    respond
   end
 
-  def hello_world
+  def respond
     counter = 0
     loop do
       request_lines = []
@@ -16,31 +18,39 @@ class Server
       while line = client.gets and !line.chomp.empty?
         request_lines << line.chomp 
       end
-      client.puts "Hello, world (#{counter})"
+      path = request_lines[0].split(' ')[1]
+      case path
+        when "/hello"
+          client.puts hello_world(counter)
+        when "/datetime"
+          client.puts datetime
+        when "/shutdown"
+          client.puts shutdown(counter)
+          client.puts diagnostics(request_lines)
+          break
+      end
       counter += 1
-      output_diagnostics(request_lines, client)
+      client.puts diagnostics(request_lines)
       client.close
     end
   end
 
-  def output_diagnostics(request_lines, client)
-    verb = request_lines[0].split(' ')[0]
-    path = request_lines[0].split(' ')[1]
-    protocol = request_lines[0].split(' ')[2]
-    host = request_lines[1].split(':')[1].strip
-    port = request_lines[1].split(':')[2]
-    accept = request_lines[6].split(':')[1].strip
-    client.puts "<pre>"
-    client.puts "Verb: #{verb}"
-    client.puts "Path: #{path}"
-    client.puts "Protocol: #{protocol}"
-    client.puts "Host: #{host}"
-    client.puts "Port: #{port}"
-    client.puts "Accept: #{accept}"
-    client.puts "Origin: #{host}"
-    client.puts "</pre>"
+  def hello_world(counter)
+    "Hello, world (#{counter})"
+  end
+
+  def datetime
+    Time.now.strftime('%I:%M on %A, %B %d, %Y') 
+  end
+
+  def shutdown(counter)
+    "Total Requests: #{counter + 1}"
+  end
+
+  def diagnostics(request_lines)
+    Diagnostics.new(request_lines).populate
   end
 
 end
 
-Server.new.hello_world
+Server.new
